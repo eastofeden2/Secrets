2a3046fd717ef29884673237038e267d16677215
junyi <junyi.chew@gmail.com>
2023-01-21 10:46:50 +0800

    bcrypt

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 //jshint esversion:6
 require('dotenv').config()
+const bcrypt = require('bcrypt');
 const express = require('express')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-var encrypt = require('mongoose-encryption');
+// var encrypt = require('mongoose-encryption');
+const md5 = require('md5')
 const app = express()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}));
 mongoose.set('strictQuery', false)
 mongoose.connect("mongodb://127.0.0.1:27017/userDB")
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -17,15 +20,19 @@ const userSchema = new mongoose.Schema({
 })
 
 const secret = process.env.SECRET
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema)
+
+
 app.post("/register", (req,res)=>{
-    const userEmail = req.body.username
-    const password = req.body.password
-    const newUser = new User({
-        email:userEmail,
-        password:password
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const userEmail = req.body.username
+        const password = hash
+        const newUser = new User({
+            email:userEmail,
+            password:password
+        
     })
     newUser.save((err)=>{
         if (err){
@@ -34,6 +41,8 @@ app.post("/register", (req,res)=>{
             res.render("secrets")
         }
     })
+    });
+    
 })
 
 app.post("/login", (req,res)=>{
@@ -43,9 +52,11 @@ app.post("/login", (req,res)=>{
         if (err){
             console.log(err);
         }else{
-            if (foundUser.password === password){
-                res.render("secrets")
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if (result==true){
+                    res.render("secrets")
+                }
+            });
         }
     })
 })
